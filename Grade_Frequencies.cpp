afeb7c48da54dc5172746899a25cb69add87427b
adamehlers <adamehlers15@gmail.com>
2023-06-03 20:20:45 -0400

    Completed getGrade function

--- Grade_Frequencies.cpp
+++ Grade_Frequencies.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+
+using namespace std;
 
 char getGrade(double score){
-
-    
+    if (score >= 90){
+        return 'A';
+    }
+    else if (score >= 80){
+        return 'B';
+    }
+    else if (score >= 70){
+        return 'C';
+    }
+    else if (score >= 60){
+        return 'D';
+    }
+    else if (score >= 0){
+        return 'F';
+    }
+    else{
+        return 'X';
+    }
 }
